1adb2f1855f40f3dda42f20e05fccbb61ce4b2d3
ismael <ismael@ismaels-MacBook-Pro.local>
2020-11-08 10:29:53 -0300

    dynamic routes

---
+++ src/pages/catalog/products/[slug].tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import  {useRouter} from 'next/router'
+// import { Container } from './styles';
+
+const Product: React.FC = () => {
+  const router = useRouter()
+  return <div ><h1>{router.query.slug}</h1></div>
+}
+
+export default Product;
---
+++ src/pages/search.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Search: React.FC = () => {
+  return <div ><h1>search</h1></div>
+}
+
+export default Search;
