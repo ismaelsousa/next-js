3f2980d66557a3b5c20a5cc3d856ca19f96f08f7
ismael <ismael@ismaels-MacBook-Pro.local>
2020-11-10 19:51:45 -0300

    client side fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,36 @@
+import { useState, useEffect } from 'react'
 import {Title} from '../styles/pages/Home'
 
+interface IProduct {
+  id:string;
+  title:string
+}
 export default function Home() {
+  const [recommendedProducts,setRecommendedProducts] = useState<IProduct[]>([])
+
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(
+      res=>res.json().then(
+        data=>setRecommendedProducts(data)
+      )
+    ) 
+  }, []);
+
   return (
-  <div><Title>hellos</Title></div>
+  <div>
+    <section>
+      <Title>Products</Title>
+      <ul>
+        {recommendedProducts.map(recommendedProduct=>{
+          return (
+            <li key={recommendedProduct.id}>
+              {recommendedProduct.title}
+            </li>
+          )
+        })}
+      </ul>
+    </section>
+  </div>
   )
 }
