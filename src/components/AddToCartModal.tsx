40a533067c7c6e5b6763cf89713fd2cf588f6bc3
ismael <ismael@ismaels-MacBook-Pro.local>
2020-11-17 22:27:57 -0300

    lazy load components

---
+++ src/components/AddToCartModal.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const AddToCartModal: React.FC = () => {
+  return (
+    <div >
+      Do you want to add to the cart?
+    </div>
+  );
+}
+
+export default AddToCartModal;
--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import  {useRouter} from 'next/router'
-// import { Container } from './styles';
+import dynamic from 'next/dynamic'
+
+const AddToCartModal = dynamic(
+  ()=>import('../../../components/AddToCartModal'),
+  {loading:()=><p>Loading...</p>, ssr:false}
+)
 
 const Product: React.FC = () => {
   const router = useRouter()
-  return <div ><h1>{router.query.slug}</h1></div>
+  const [isAddToCartModalVisible,setIsAddToCartModalVisible] = useState(false)
+
+  const handleAddToCart = useCallback(()=>{
+    setIsAddToCartModalVisible(old=>!old)
+  },[])
+  return (
+    <div>
+      <h1>{router.query.slug}</h1>
+      <button onClick={handleAddToCart}>Add to cart</button>
+      {isAddToCartModalVisible&& <AddToCartModal/>}
+    </div>
+  )
 }
 
 export default Product;
